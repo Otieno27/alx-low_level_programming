f9d35ad8b2a02cfe470914cd0441665ea1bdfc75
Otieno27 <mosesotieno725@gmail.com>
2022-10-18 13:36:33 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,6 @@
  * main - this is prints programming is like building
  *        a multilingual puzzle
  *  
- * 
  * Return - Always 0
  */       
 int main(void)
@@ -12,6 +11,5 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
-
 }	
 
