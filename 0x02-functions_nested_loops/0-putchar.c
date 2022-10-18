f90a14d3e8cfed03ec7e2a5bec6d6d3b6fe249cb
Otieno27 <mosesotieno725@gmail.com>
2022-10-18 08:06:19 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Main - The program prints _putchar
+ *        followed by a new line
+ *
+ * Return - Always 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
