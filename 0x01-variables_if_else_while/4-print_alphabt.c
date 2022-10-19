8cb5983e9d118b75baa109e536386e55603ca898
Otieno27 <mosesotieno725@gmail.com>
2022-10-19 02:19:19 -0700

    Variables, if else and while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase, then a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{	
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if(letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0|);
+}
