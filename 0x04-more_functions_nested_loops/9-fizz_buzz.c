69eee0daddcfb462dc95383f62220b1bd50ee627
Otieno27 <mosesotieno725@gmail.com>
2022-10-24 13:49:36 -0700

    Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,33 +2,25 @@
 
 /**
  * main - fizzbuzz challenge
- *
  * Return: Always 0
  */
 int main(void)
 {
-	int num;
+	int n;
 
-	for (num = 1; num <= 100; num++)
+	printf("1");
+	for (n = 2; n <= 100; n++)
 	{
-		if (num % 3) == 0 && (num % 5) == 0)
-			printf("FizzBuzz");
-
-		else if ((num % 3) == 0)
+		printf(" ");
+		if (n % 3 == 0)
 			printf("Fizz");
 
-		else if ((num % 5) == 0)
+		if (n % 5 == 0)
 			printf("Buzz");
 
-		else
-			printf("%d", num);
-
-		if (num == 100)
-			continue;
-		printf(" ");
+		if (n % 3 != 0 && n % 5 != 0)
+			printf("%d", n);
 	}
-
 	printf("\n");
-
 	return (0);
 }
