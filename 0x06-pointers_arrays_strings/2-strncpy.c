835a0057647931f71fcbe9ef91c581ff121e576e
Otieno27 <mosesotieno725@gmail.com>
2022-10-28 14:57:07 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _strncpy - copys a string with n
@@ -9,19 +10,6 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
-
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-		dest[j] = src[j];
-		j++;
-	}
-	while (j < n)
-	{
-		dest[j] = '\0';
-		j++;
-	}
-
+	strncpy(dest, src, n);
 	return (dest);
 }
