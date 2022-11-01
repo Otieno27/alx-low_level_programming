3f49d812df58aa8497e4c84cfab5bc6069a8f1a7
Otieno27 <mosesotieno725@gmail.com>
2022-11-01 18:00:59 +0000

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,14 +9,12 @@
  *
  * Return: pointer to the destination
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
 
 	for (i = 0; i < n; i++)
-		destination[i] = source[i];
-
+		dest[i] = src[i];
+	
 	return (dest);
 }
