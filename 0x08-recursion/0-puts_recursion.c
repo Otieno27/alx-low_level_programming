f75e9bcf2fa2c58d98740bc7711b3c68314c1206
Otieno27 <mosesotieno725@gmail.com>
2022-11-03 16:34:13 +0000

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts_recursion - prints a string
+ * @s: string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
