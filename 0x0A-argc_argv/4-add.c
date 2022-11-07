4de51a9fc4699d7b64973670bea4ec635e999f9e
Otieno27 <mosesotieno725@gmail.com>
2022-11-07 14:55:37 +0000

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - prints the addition of positive numbers, 
+ * 	followed by a new line
+ * @argc: counts argument
+ * @argv: argument
+ *
+ * Return: if number contains only digits 0, else 1
+ */
+int main(int argc, char *argv[])
+{
+int num, sum = 0;
+
+for (num = 1; num < argc; num++)
+{
+if (atoi(argv[num]) != 0)
+sum += atoi(argv[num]);
+
+else
+{
+printf("Error\n");
+return (1);
+}
+}
+
+printf("%d\n", sum);
+
+return (0);
+}
