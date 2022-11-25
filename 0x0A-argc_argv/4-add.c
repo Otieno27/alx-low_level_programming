5a0ba1841213dd9a2a7398240064f8f9f5c6fca2
Otieno27 <mosesotieno725@gmail.com>
2022-11-25 20:16:43 +0000

    Task 4 correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,21 +11,21 @@
  */
 int main(int argc, char *argv[])
 {
-int num, sum = 0;
+	int num, sum = 0;
 
-for (num = 1; num < argc; num++)
-{
-if (atoi(argv[num]) != 0)
-sum += atoi(argv[num]);
+	for (num = 1; num < argc; num++)
+	{
+		if (atoi(argv[num]) != 0)
+			sum += atoi(argv[num]);
 
-else
-{
-printf("Error\n");
-return (1);
-}
-}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
 
-printf("%d\n", sum);
+	printf("%d\n", sum);
 
-return (0);
+	return (0);
 }
